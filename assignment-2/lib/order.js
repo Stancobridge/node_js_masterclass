9b5a308b50c4c25ece14a580647aeb70eb9e83e8
Okechukwu Somtochukwu <stancoproof@gmail.com>
2019-03-13 01:40:02 +0100

    Update order.js

--- assignment-2/lib/order.js
+++ assignment-2/lib/order.js
@@ -6,30 +6,11 @@ order = {};
 
 order.makeAnOrder = (customerEmail, paymentDetailsData, cartData, callback) => {
     // Get total price
-    console.table(cartData);
     let totalPrice = cartData.reduce((carr, curr) => carr + (Number(curr.item_price).valueOf() * curr.count), 0)
-
-    console.log(totalPrice);
-    // Create Order Object
-    // let transaction_id = helpers.createRandomString(12);
-    // let orderData = {
-    //     transaction_id,
-    //     customerEmail,
-    //     items : cartData,
-    //     totalPrice,
-    //     paymentDetailsData
-    // }
-
-    // "card_number": "4gdsjhgsdfdsa",
-    // "card_exp_month": "4gdsjhgsdfdsa",
-    // "card_exp_year": "4gdsjhgsdfdsa",
-    // "card_cvc": "4gdsjhgsdfdsa"
-    // Create Payment Details
     
     stripe.createPaymentToken(paymentDetailsData, (err, tokenId) => {
         if(!err){
             stripe.makePayment(tokenId, totalPrice, (err, paymentId) => {
-                console.log(paymentId);
                 if(!err && paymentId){
                     
                     // Add payment id to the cartData
